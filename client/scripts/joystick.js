8d7c8e9f8ab489768cf4260acbb070fd064792ac
Grzes <stuczynski.g@gmail.com>
2017-02-06 22:30:34 +0100

    sockets tests

--- client/scripts/joystick.js
+++ client/scripts/joystick.js
@@ -4,11 +4,14 @@ var options = {
     };
 var manager = nipplejs.create(options);
 
+var socket = io.connect('/');
 
 manager
 .on('added',  (evt, nipple) => console.log("start"))
 .on('removed', (evt, nipple) => console.log("remove"))
 .on('dir:left', (evt, nipple)=> console.log("left"))
 .on('dir:right', (evt, nipple)=> console.log("right"))
-.on('dir:up', (evt, nipple)=> console.log("up"))
+.on('dir:up', (evt, nipple)=> {
+    socket.emit('forward');
+})
 .on('dir:down', (evt, nipple)=> console.log("down"));
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const PORT = 3000;
 
+
 var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+var piSocket = require('socket.io-client')//('http://192.168.0.14:3005',{reconnect: true});
+
 
 var mainRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 
-var app = express();
+
 
 app.use('/', mainRouter);
 app.use('/api', apiRouter);
@@ -17,6 +23,27 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/client'));
 
 
-app.listen(PORT, function(){
+
+
+server.listen(PORT, ()=>{
   console.log('Listening on port: '+ PORT);
+});
+
+
+ var pi = piSocket.connect('http://192.168.0.14:3013')
+//console.log(pi)
+pi.once("connect",()=>{
+  console.log("c")
+pi.emit('test',{x:'x'},(msg)=>console.log(msg+"msg"))
+})
+   // piSocket.emit('forward',{},(data, err)=> console.log(data+err))
+
+io.on("connection", (socket)=>{
+
+
+  console.log("client connected");
+  socket.on("forward",(state)=>{
+  
+    console.log("poszedl x");
+  })
 });
