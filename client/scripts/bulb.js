a0012500e0cc0f391a5cc7eea8cdee68fd295ed3
Grzegorz Stuczyński <grzegorz.stuczynski@evidenceprime.com>
2017-06-03 13:20:04 +0200

    code review

--- client/scripts/bulb.js
+++ client/scripts/bulb.js
@@ -2,20 +2,9 @@ var xhr = new XMLHttpRequest();
 
 var bulbOnBtn = document.querySelector("#bulbOn");
 var bulbOffBtn = document.querySelector("#bulbOff");
-
-function bulbOn(){
-    console.log(xhr.readyState)
-    xhr.open("GET","http://192.168.0.14:3005/blink", true);
-    console.log(xhr.readyState);
-    xhr.send();
-}
-
-
-bulbOnBtn.addEventListener("click",()=>{
+bulbOnBtn.addEventListener("click", () => {
     socket.emit('bulbOn');
-    console.log("bulbOn")
 }, false);
-bulbOffBtn.addEventListener("click",()=>{
+bulbOffBtn.addEventListener("click", () => {
     socket.emit('bulbOff');
-    console.log("bulbOff")
 }, false);
--- client/scripts/joystick.js
+++ client/scripts/joystick.js
@@ -1,31 +1,24 @@
+var joystick = new VirtualJoystick({
+  container: document.getElementById('zone_joystick'),
+  mouseSupport: true
+});
 
-  
-  var joystick  = new VirtualJoystick({
-    container : document.getElementById('zone_joystick'),
-    mouseSupport : true
-  });
-
-  function motion() {
-    if( joystick.up() ) {
-      socket.emit('forward');
-      console.log("forward")
-    }
-    if( joystick.down() ) {
-      socket.emit('reverse');
-      console.log("reverse");
-    }
-    if( joystick.right() ) {
-      socket.emit('right');
-      console.log("right");
-    } 
-    if( joystick.left() ) {
-      socket.emit('left');
-      console.log("left");
-    }
-    if( joystick.deltaX() === 0 && joystick.deltaY() === 0 ) {
-      //socket.emit('stop');
-     // console.log("stop")
-    }
-    requestAnimationFrame(motion);
+function motion() {
+  if (joystick.up()) {
+    socket.emit('forward');
+  }
+  if (joystick.down()) {
+    socket.emit('reverse');
+  }
+  if (joystick.right()) {
+    socket.emit('right');
+  }
+  if (joystick.left()) {
+    socket.emit('left');
+  }
+  if (joystick.deltaX() === 0 && joystick.deltaY() === 0) {
+    socket.emit('stop');
   }
   requestAnimationFrame(motion);
+}
+requestAnimationFrame(motion);
